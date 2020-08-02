2f5a6e7503f5c86a17d1cb847af82e2eea523563
chenzuoting <chenzuotinglearn@gmail.com>
2020-08-02 00:42:07 -0700

    login, logout, signup

--- Insta/views.py
+++ Insta/views.py
@@ -2,34 +2,55 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from Insta.models import InstaUser, Post
+from Insta.forms import CustomUserCreationForm
 
 # Create your views here.
 
 class HelloWorld(TemplateView):
     template_name = 'helloworld.html'
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'home.html'
+    login_url = 'login'
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
+    login_url = 'login'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
-    fields = '__all__'
+    fields = ['title', 'image']
+    login_url = 'login'
 
-class PostUpdateView(UpdateView):
+    # Manually set author here
+    # This method is called when valid form data has been POSTed.
+    # It should return an HttpResponse.
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_update.html'
     fields = ['title']
+    login_url = 'login'
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     #cannot use reverse here, we are trying to redirect during delete process, which is invalid
     #with reverse_lazy, we are redirecting and let delete running in the back
     success_url = reverse_lazy("home")
+    login_url = 'login'
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
+
+    
