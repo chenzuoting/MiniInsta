17287e70eb8bb5e4ef58d5448949f628ea6cde03
chenzuoting <chenzuotinglearn@gmail.com>
2020-08-03 09:46:11 -0700

    CSS

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from Insta.models import InstaUser, Post
+from Insta.models import InstaUser, Post, UserConnection, Like
 
 # Register your models here.
 
 admin.site.register(InstaUser)
 admin.site.register(Post)
+admin.site.register(UserConnection)
+admin.site.register(Like)
--- Insta/models.py
+++ Insta/models.py
@@ -36,3 +36,43 @@ class Post (models.Model):
 
     def get_absolute_url(self): #called when new post saved
         return reverse("post_detail", args=[str(self.id)]) #get url based on name
+
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set"
+    )
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set"
+    )
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        #related_name: define here so that its foreign key object, post, can visit it  with this name 
+        #and get all the connections with current post
+        related_name='likes'
+    )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    class Mata:
+        # Use this in a relation model, determine if we need some unique info 
+        # Combination post and user should be unique
+        unique_together = ("post", "user")
+
+    # Display as string
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
+    
